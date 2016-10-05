641a51011dcf71ecec612be7b3d5f24ae3c157f8
Drew Easley <develop.github@juneeighteen.com>
2016-10-05 10:07:28 -0400

    Updated create-jenkins-user to not git pull

--- src/bin/create-jenkins-user.sh
+++ src/bin/create-jenkins-user.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-mkdir -p ~/projects
-cd ~/projects
-rm -rf ezriopen-jenkins
-git clone https://github.com/ezricloud/ezriopen-jenkins
 adduser jenkins --disabled-password --shell /bin/bash --home /home/jenkins --gecos ""
-cp -R ~/projects/ezriopen-jenkins/src /home/jenkins
+cp -R ezriopen-jenkins/src/* /home/jenkins
 chown -R jenkins:jenkins /home/jenkins
